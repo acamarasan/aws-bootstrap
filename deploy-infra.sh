7b47296174e14a728255b733b978379b11f32090
Andrei Camarasan <andrei.camarasan@mindmagnetsoftware.com>
2021-02-25 13:46:06 +0200

    Add CodeBuild / CodeDeploy spec.

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,21 @@ REGION=eu-west-1
 CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -17,3 +32,9 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE
+
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
