74119f3bc8278e61cd3a3d545a475d45d927f9f1
Andrei Camarasan <andrei.camarasan@mindmagnetsoftware.com>
2021-02-26 13:34:49 +0200

    Add second instance.

--- delete-infra.sh
+++ delete-infra.sh
@@ -5,11 +5,11 @@ REGION=eu-west-1
 CLI_PROFILE=awsbootstrap
 
 # Delete static resources
-echo -e "\n\n=========== Deleting static resources ==========="
-aws cloudformation delete-stack \
-  --region $REGION \
-  --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME-setup
+# echo -e "\n\n=========== Deleting static resources ==========="
+# aws cloudformation delete-stack \
+#   --region $REGION \
+#   --profile $CLI_PROFILE \
+#   --stack-name $STACK_NAME-setup
 
 # Delete the CloudFormation template
 echo -e "\n\n=========== Deleting infrastructure ==========="
